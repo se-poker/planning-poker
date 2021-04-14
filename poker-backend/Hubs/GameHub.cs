9f84f234b4e68c0ad5bf4b2b5b0147d5431ab765
Korneliusz Szymański <colorgreen19@gmail.com>
2021-04-14 09:02:38 +0200

    Backend

---
+++ poker-backend/Hubs/GameHub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.SignalR;
+using poker_backend.Managers;
+
+namespace poker_backend.Hubs
+{
+    public class GameHub : Hub
+    {
+        private GamesManager GamesManager = new();
+        
+        private string Connection => Context.ConnectionId;
+
+        public async Task JoinGame(string gamename, string username)
+        {
+            var game = GamesManager.JoinGame(Connection, gamename, username);
+
+            await Groups.AddToGroupAsync(Connection, gamename);
+            
+            await Clients.Group(game.Name).SendAsync("Game", game);
+        }
+
+        public async Task VoteCard(int card)
+        {
+            var game = GamesManager.GetGameByConnection(Connection);
+            game.VoteCard(Connection, card);
+
+            await Clients.Group(game.Name).SendAsync("OnVotedCard", Connection);
+        }
+
+        public async Task ShowCards()
+        {
+            var game = GamesManager.GetGameByConnection(Connection);
+            game.ShowCards();
+       
+            await Clients.Group(game.Name).SendAsync("Game", game);
+        }
+
+        public async Task NewRound()
+        {
+            var game = GamesManager.GetGameByConnection(Connection);
+            game.ClearVotes();
+            
+            await Clients.Group(game.Name).SendAsync("Game", game);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var game = GamesManager.GetGameByConnection(Connection);
+            game.DeleteUser(Connection);
+            
+            await Clients.Group(game.Name).SendAsync("OnUserDeleted", Connection);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
